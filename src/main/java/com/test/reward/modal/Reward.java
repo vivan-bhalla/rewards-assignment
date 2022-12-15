e9b281300f70567ff3fd943fac6d8d0cad5dfdda
vivan-bhalla <55252892+vivan-bhalla@users.noreply.github.com>
2022-12-15 08:02:07 +0530

    Create Reward.java

---
+++ src/main/java/com/test/reward/modal/Reward.java
@@ -0,0 +1,29 @@
+package com.test.reward.modal;
+
+public class Reward {
+	private int amount;
+	private String date;
+	private String customerName;
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+
+	
+
+}
