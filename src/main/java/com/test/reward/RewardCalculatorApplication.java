9ec1fcaf7e5d14b33996fbe0e43051be5b15c020
vivan-bhalla <55252892+vivan-bhalla@users.noreply.github.com>
2022-12-15 08:00:32 +0530

    Create RewardCalculatorApplication.java

---
+++ src/main/java/com/test/reward/RewardCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.test.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardCalculatorApplication.class, args);
+	}
+
+}
